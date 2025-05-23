bd4656a5b0c64807833529fe98d822719735def9
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-05-23 23:32:54 +0300

    Add task

--- index.js
+++ index.js
@@ -37,3 +37,25 @@ async function getProduct() {
   }
 }
 getProduct();
+
+//============================================
+//HomeTask
+
+const product1 = {
+  id: 0,
+  title: "string",
+  price: 0.1,
+  description: "string",
+  category: "string",
+  image: "http://example.com",
+};
+
+fetch("https://fakestoreapi.com/products", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify(product1),
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data));
