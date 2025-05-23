769abbe3eb27eb174b71af4b32c708c6ed1cfbb7
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-05-23 22:47:23 +0300

    GET запит,POST  запит

---
+++ index.js
@@ -0,0 +1,26 @@
+// Приклад GET запит
+fetch("https://fakestoreapi.com/products")
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+//==========================================
+
+//Приклад POST запит
+
+const product = {
+  id: 0,
+  title: "string",
+  price: 0.1,
+  description: "string",
+  category: "string",
+  image: "http://example.com",
+};
+
+fetch("https://fakestoreapi.com/products", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify(product),
+})
+  .then(response => response.json())
+  .then(data => console.log(data));
